def11dfc0abe77820610b1b938802bbef55de55e
eddah73 <eddahsjr@gmail.com>
2024-07-23 18:53:02 +0300

    commits

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {//n
+    for (let j = i + 1; j < array.length; j++) {//n
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  nested loop
+  O(n**2)
 */
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(arrayofinteger,target){
+  iterate through integers of array adding number
+  if(addedNum===target){
+  return true
+}
+  else{
+  return false
+  }}
+  
 */
 
 /*
   Add written explanation of your solution here
+  function hasTargetSum
+  takes two argument(arrayof integers,target)
+  add two pairs of integers in array and compare it with target
+  if added pair  and target match return true
 */
 
 // You can run `node index.js` to view these console logs
